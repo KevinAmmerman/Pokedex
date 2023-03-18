29df224f0e9b5fe4cd2dbcead11cc36eacd81898
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-03-18 18:55:49 +0100

    wrote functions for rendering the small cards.

--- src/scripts/htmlTemplates.js
+++ src/scripts/htmlTemplates.js
@@ -1,11 +1,26 @@
-function renderPokemon() {
+function createHtmlForPokemonSmallCard(currentPokemon, i) {
+    let pokemonName = capitalizeFirstLetter(currentPokemon.name);
+    let pokemonType = currentPokemon.types[0].type.name;
+    // let pokemonType2 = checkType(currentPokemon);
+    let pokemonNumber = formatNumber(currentPokemon.id);
+    let pokemonImage = currentPokemon.sprites.other.dream_world.front_default;
+    let actualColor = setPokemonListCardsBgr(currentPokemon.types[0].type.name);
     return `
-        <div class="pokemonCardSmall">
-            <h1 class="pokeName">${capitalizeFirstLetter(allPokemon)}</h1>
-            <div class="typ">${allPokemon.types[0].type.name}</div>
-            <div class="orderPokemon">${formatNumber(allPokemon)}</div>
-            <img class="pokeImage" src="${allPokemon.sprites.other.dream_world.front_default}" alt="">
+        <div class="pokemonCardSmall" style="background-color: ${actualColor}">
+            <h1 class="pokeName">${pokemonName}</h1>
+            <div class="typ">${pokemonType}</div>
+            <div class="orderPokemon">${pokemonNumber}</div>
+            <img class="pokeImage" src="${pokemonImage}" alt="">
             <img class="pokeballImage" src="src/img/Unbenannt.png" alt="">
         </div>
     `;
 }
+
+
+// function checkType(currentPokemon) {
+//     if (Array.isArray(currentPokemon.types[1])) {
+//         return currentPokemon.types[1].type.name;
+//     } else {
+//         return '';
+//     }
+// }
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,24 +1,62 @@
-let responeAsJson;
+let start = 1;
+let endOf = 50;
+
 let allPokemon = [];
 
 
+async function init() {
+    await loadPokemon();
+    renderPokemon();
+}
+
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0';
-    let response = await fetch(url);
-    responeAsJson = await response.json();
-    for (let i = 0; i < responeAsJson.results.length; i++) {
-        const pokemon = responeAsJson.results[i];
-        let url = pokemon.url;
+    for (let i = start; i <= endOf; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
-        allPokemon = await response.json();
-        document.getElementById('cardContainer').innerHTML += renderPokemon();
+        let currentPokemon = await response.json();
+        allPokemon.push(currentPokemon);
     }
 }
+
+
+function renderPokemon() {
+    for (let i = 0; i < allPokemon.length; i++) {
+        const currentPokemon = allPokemon[i];
+        document.getElementById('cardContainer').innerHTML += createHtmlForPokemonSmallCard(currentPokemon, i);
+    }
+}
+
+
+function setPokemonListCardsBgr(element) {
+    return (
+        {
+            fire: "#E53B18",
+            grass: "#59A952",
+            water: "#278BCC",
+            poison: "#864AB8",
+            flying: "#87B5E5",
+            normal: "#A8A899",
+            bug: "#83AD25",
+            dark: "#463D3E",
+            dragon: "#4D64AB",
+            electric: "#E5C600",
+            fairy: "#FFBBF9",
+            fighting: "#D580CF",
+            ghost: "#633C64",
+            ground: "#966833",
+            ice: "#68BAAC",
+            psychic: "#E55973",
+            rock: "#A8995B",
+            steel: "#9999A8",
+        }[element.toLowerCase()] || "#5D9081"
+    );
+}
+
 
 // This function takes a Pokemon object as input and returns a formatted ID string.
 
 function formatNumber(currentPokemon) {
-    let num = currentPokemon.id;
+    let num = currentPokemon;
     if (num < 10) {
         return '#00' + num;
     } else if (num < 100) {
@@ -32,6 +70,6 @@ function formatNumber(currentPokemon) {
 // This function takes a Pokemon object as input and returns the name of the Pokemon with the first letter capitalized.
 
 function capitalizeFirstLetter(currentPokemon) {
-    let upCase = currentPokemon.name;
+    let upCase = currentPokemon;
     return upCase.charAt(0).toUpperCase() + upCase.slice(1);
 }
