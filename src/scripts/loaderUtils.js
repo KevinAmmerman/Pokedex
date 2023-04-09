a946485a46ea213cd8bab0eb04929c2700c8e3e3
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-04-09 18:13:33 +0200

    #

--- src/scripts/loaderUtils.js
+++ src/scripts/loaderUtils.js
@@ -8,3 +8,37 @@ function hideLoader() {
     const loader = document.getElementById("loadingContainer");
     loader.style.display = "none";
 }
+
+
+// window.addEventListener('scroll', function () {
+//     const current = window.innerHeight + this.window.scrollY;
+//     const maxHeight = document.body.scrollHeight;
+//     console.log('check current', current);
+//     console.log('check max', maxHeight);
+//     if (current -24 == maxHeight) {
+//         moreCards();
+//       }
+// });
+
+// function scrollHandler() {
+//     const current = window.innerHeight + window.scrollY;
+//     const maxHeight = document.body.scrollHeight;
+//     console.log('check current', current);
+//     console.log('check max', maxHeight);
+//     if (current -24 == maxHeight) {
+//         moreCards();
+//     }
+// }
+
+// let cardContainer = document.getElementById('cardContainer');
+// console.log('cardContainer:', cardContainer);
+
+// document.getElementById('cardContainer').addEventListener('scroll', function () {
+//     const current = cardContainer.scrollTop + cardContainer.clientHeight;
+//     const maxHeight = cardContainer.scrollHeight;
+//     console.log('check current', current);
+//     console.log('check max', maxHeight);
+//     if (current === maxHeight) {
+//         console.log('Ende des Scrollens erreicht.');
+//     }
+// });
